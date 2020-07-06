48004c6b103ce0471383bfd7b3abff210e57f374
雨が好きな人 <41268920+A-Rain-Lover@users.noreply.github.com>
2020-07-06 23:47:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -18,28 +18,28 @@ const mod = (n, k) => {
 }
 
 /* Modular distance */
-const mod_dist = (x, y)=> {
-    return min(mod((x - y),60),mod((y - x),60));
+const mod_dist = (x, y, modulo)=> {
+    return min(mod((x - y),modulo),mod((y - x),modulo));
 }
 
 /* Draws a circle of numbers 
  *  radius : self explanatory
  *  n      : the highlighted number 
  */
-const num_circle = (n, radius) => {
-  if(n > 59) return null; /* Unless you are not on earth */
+const num_circle = (n, max, radius) => {
+  if(n > max) return null; /* Unless you are not on earth */
   
   push();
   
   /* Rotate to the current number */
-  rotate(2 * n * Math.PI / 60);
+  rotate(2 * n * Math.PI / max);
   
   let i = -1;
-  while(i++ < 59){ /* Print all numbers */
+  while(i++ < max - 1){ /* Print all numbers */
     
     /* Just for the styling */
-    let distance = mod_dist(n,i);
-    let mapped_d = map(distance,0,30,0,255);
+    let distance = mod_dist(n,i, max);
+    let mapped_d = map(distance,0,max/2,0,255);
     
     /* Playing with colors */
     fill(50, 156-pow(mapped_d, 2), 111, 255-mapped_d);
@@ -48,7 +48,7 @@ const num_circle = (n, radius) => {
     textSize(20-map(mapped_d,0,255,0,20));
     
     text(String(i).padStart(2, "0"),radius,0);
-    rotate(-2*Math.PI/60);
+    rotate(-2*Math.PI/max);
   }
   
   pop();
@@ -80,9 +80,9 @@ function draw() {
   translate(inner_radius - margin, height/2);
   
   /* The 3 rings */
-  num_circle(h, inner_radius + margin * 0); /* Yes, I am weired */
-  num_circle(m, inner_radius + margin * 1); 
-  num_circle(s, inner_radius + margin * 2); 
+  num_circle(h, 24, inner_radius + margin * 0); /* Yes, I am weired */
+  num_circle(m, 60, inner_radius + margin * 1); 
+  num_circle(s, 60, inner_radius + margin * 2); 
   
   line(inner_radius-25,-5,inner_radius-5,-5);
   line(inner_radius+2*margin+25,-5,inner_radius+2*margin+25+20,-5);
